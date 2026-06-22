13a4f1e36cc86f40d2d77f7ca7a134d94522670d
Vincent Billard <vincent.billard@edifice.io>
2026-06-22 11:37:49 +0200

    feat: #IMPULS-5955 add test utils for test mass configuration of structure

--- test/src/main/java/org/entcore/test/preparation/StructureTest.java
+++ test/src/main/java/org/entcore/test/preparation/StructureTest.java
@@ -5,17 +5,21 @@ public class StructureTest {
     private final String id;
     private final String name;
     private final boolean defaultAuthFederated;
+    private final String uai;
 
     public StructureTest(final String id, final String name) {
-        this.id = id;
-        this.name = name;
-        this.defaultAuthFederated = false;
+        this(id, name, false, null);
     }
 
     public StructureTest(final String id, final String name, boolean defaultAuthFederated) {
+        this(id, name, defaultAuthFederated, null);
+    }
+
+    public StructureTest(final String id, final String name, boolean defaultAuthFederated, final String uai) {
         this.id = id;
         this.name = name;
         this.defaultAuthFederated = defaultAuthFederated;
+        this.uai = uai;
     }
 
     public String getId() {
@@ -28,6 +32,10 @@ public class StructureTest {
 
     public boolean isDefaultAuthFederated() {
         return defaultAuthFederated;
+    }
+
+    public String getUai() {
+        return uai;
     }
 
 

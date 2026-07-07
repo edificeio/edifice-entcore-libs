2614c2bf6adf4bbbe707d7ce11f9589817757d0d
Junior Bernard <junior.bernard@opendigitaleducation.com>
2026-07-07 21:31:24 +0200

    feat: #RBACK-238, java 21 migration

--- common/src/main/java/org/entcore/common/s3/utils/MultipartUpload.java
+++ common/src/main/java/org/entcore/common/s3/utils/MultipartUpload.java
@@ -6,10 +6,10 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
 
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpMethod;

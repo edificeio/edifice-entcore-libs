ebfb313a904654bda7355be5f433b1b71747d2e3
Vincent Billard <vincent.billard@edifice.io>
2026-09-04 15:08:49 +0200

    fix: #IMPULS-6176 add options to visibile identity query

---
+++ common/src/main/java/org/entcore/common/user/dto/VisibleIdentityRequest.java
@@ -0,0 +1,104 @@
+package org.entcore.common.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class VisibleIdentityRequest {
+
+
+    private String userId;
+    private boolean itSelf;
+    private boolean includeHiddenCommunity;
+    private List<String> expectedVisiblesIds;
+    private List<String> includedVisibleIds;
+    private boolean publicDetails;
+    private String search;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     * Id of the user doing the visible request
+     * @param userId
+     * @return
+     */
+    public VisibleIdentityRequest setUserId(String userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    /**
+     * Include himself in the response
+     * @return
+     */
+    public boolean isItSelf() {
+        return itSelf;
+    }
+
+    public VisibleIdentityRequest setItSelf(boolean itSelf) {
+        this.itSelf = itSelf;
+        return this;
+    }
+
+    /**
+     * Include hidden group use by Communities
+     * @return
+     */
+    public boolean isIncludeHiddenCommunity() {
+        return includeHiddenCommunity;
+    }
+
+    public VisibleIdentityRequest setIncludeHiddenCommunity(boolean includeHiddenCommunity) {
+        this.includeHiddenCommunity = includeHiddenCommunity;
+        return this;
+    }
+
+    /**
+     * Restrict the response to those user or group ids. Null or empty means no restriction : every visible
+     * of the user is returned.
+     * @return
+     */
+    public List<String> getExpectedVisiblesIds() {
+        return expectedVisiblesIds;
+    }
+
+    public VisibleIdentityRequest setExpectedVisiblesIds(List<String> expectedVisiblesIds) {
+        this.expectedVisiblesIds = expectedVisiblesIds;
+        return this;
+    }
+
+    /**
+     * Return only id + type of visible, or add public general information (dislayName / name / groupName
+     *  structureName, profile
+     * @return
+     */
+    public boolean isPublicDetails() {
+        return publicDetails;
+    }
+
+    public VisibleIdentityRequest setPublicDetails(boolean publicDetails) {
+        this.publicDetails = publicDetails;
+        return this;
+    }
+
+    public List<String> getIncludedVisibleIds() {
+        return includedVisibleIds;
+    }
+
+    public VisibleIdentityRequest setIncludedVisibleIds(List<String> includedVisibleIds) {
+        this.includedVisibleIds = includedVisibleIds;
+        return this;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public VisibleIdentityRequest setSearch(String search) {
+        this.search = search;
+        return this;
+    }
+}
